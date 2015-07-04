5279fe20900e3c57989aee3b6da4810f9de6364f
RJ Robinson <robertj.robinson@gmail.com>
2015-07-04 12:09:36 -0400

    updated application controller to allow username regerstration

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -35,26 +35,26 @@ flapperNews.config([
                     }]
                 }
             })
-            // .state('login', {
-            //     url: '/login',
-            //     templateUrl: 'auth/_login.html',
-            //     controller: 'AuthCtrl',
-            //     onEnter: ['$state', 'Auth', function($state, Auth) {
-            //         Auth.currentUser().then(function() {
-            //             $state.go('home');
-            //         })
-            //     }]
-            // })
-            // .state('register'ontroller
-            //     url: '/register',
-            //     templateUrl: 'auth/_register.html',
-            //     controller: 'AuthCtrl',
-            //     onEnter: ['$state', 'Auth', function($state, Auth) {
-            //         Auth.currentUser().then(function() {
-            //             $state.go('home');
-            //         })
-            //     }]
-            // });
+            .state('login', {
+                url: '/login',
+                templateUrl: 'auth/_login.html',
+                controller: 'AuthController',
+                onEnter: ['$state', 'Auth', function($state, Auth) {
+                    Auth.currentUser().then(function() {
+                        $state.go('home');
+                    })
+                }]
+            })
+            .state('register', {
+                url: '/register',
+                templateUrl: 'auth/_register.html',
+                controller: 'AuthController',
+                onEnter: ['$state', 'Auth', function($state, Auth) {
+                    Auth.currentUser().then(function() {
+                        $state.go('home');
+                    })
+                }]
+            });
 
         $urlRouterProvider.otherwise('home');
     }
